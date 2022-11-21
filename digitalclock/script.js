dd0f8e3606e3b2830ae03e7ac77612db4c8c6aa9
VigneshhVikki <111519596+VigneshhVikki@users.noreply.github.com>
2022-11-21 21:53:55 +0530

    Add files via upload

---
+++ digitalclock/script.js
@@ -0,0 +1,30 @@
+function showTime() {
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes=date.getMinutes();
+    let seconds=date.getSeconds();
+    let session = "AM";
+    if(hours > 12) {
+    hours = hours -12;
+    session = "PM";
+   }
+
+   //Ternary operator condion ? true : false
+   hours = hours <10 ? "0" + hours : hours;
+   minutes = minutes <10 ? "0" + minutes : minutes;   
+   seconds = seconds <10 ? "0" + seconds : seconds;   
+
+   let time = hours + ":" + minutes + ":" + seconds + session;
+   document.getElementById("clock").innerText = time;   
+
+   setTimeout(showTime, 1000);
+}  
+
+showTime();
+
+// setInterval(() => {
+    // console.log("Hitting every seconds");
+    // showTime(); 
+// }, 1000);
+
+// setInterval(showTime, 1000);
